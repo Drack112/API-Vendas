dc2b5ecafc005bddb937b29a6d69922305adc8e2
Drack <61944386+Drack112@users.noreply.github.com>
2022-08-21 16:58:55 -0300

    :orange: validation and fix some errors on statusCode

--- src/modules/products/routes/products.routes.ts
+++ src/modules/products/routes/products.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { celebrate, Joi, Segments } from "celebrate";
 
 import ProductsController from "../controllers/ProductsController";
 
@@ -6,9 +7,52 @@ const productsRouter = Router();
 const productsController = new ProductsController();
 
 productsRouter.get("/", productsController.index);
-productsRouter.get("/:id", productsController.show);
-productsRouter.post("/", productsController.create);
-productsRouter.put("/:id", productsController.update);
-productsRouter.delete("/:id", productsController.delete);
+
+productsRouter.get(
+  "/:id",
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+  }),
+  productsController.show,
+);
+
+productsRouter.post(
+  "/",
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      price: Joi.number().precision(2).required(),
+      quantity: Joi.number().required(),
+    },
+  }),
+  productsController.create,
+);
+
+productsRouter.put(
+  "/:id",
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+    [Segments.BODY]: {
+      name: Joi.string(),
+      price: Joi.number().precision(2),
+      quantity: Joi.number(),
+    },
+  }),
+  productsController.update,
+);
+
+productsRouter.delete(
+  "/:id",
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+  }),
+  productsController.delete,
+);
 
 export default productsRouter;
--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,8 +1,10 @@
 import "reflect-metadata";
 import "@shared/typeorm";
+import "express-async-errors";
 
 import express, { NextFunction, Request, Response } from "express";
 import cors from "cors";
+import { errors } from "celebrate";
 
 import routes from "./routes";
 import AppError from "@shared/errors/AppError";
@@ -13,6 +15,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use(routes);
+app.use(errors());
 
 app.use(
   (error: Error, request: Request, response: Response, next: NextFunction) => {

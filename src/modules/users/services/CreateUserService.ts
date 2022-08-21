03f8497423d5c5f2c74523c98f45d1f918126510
Drack <61944386+Drack112@users.noreply.github.com>
2022-08-21 20:37:43 -0300

    :coffee: Sessions

--- src/modules/users/services/CreateUserService.ts
+++ src/modules/users/services/CreateUserService.ts
@@ -1,4 +1,5 @@
 import AppError from "@shared/errors/AppError";
+import { hash } from "bcrypt";
 import { getCustomRepository } from "typeorm";
 import User from "../typeorm/entities/User";
 import UsersRepository from "../typeorm/repositories/UserRepository";
@@ -18,10 +19,12 @@ class CreateUserService {
       throw new AppError("Email address already used.");
     }
 
+    const hashedPassword = await hash(password, 8);
+
     const user = usersRepository.create({
       name,
       email,
-      password,
+      password: hashedPassword,
     });
 
     await usersRepository.save(user);
